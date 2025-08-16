3e59106e2874389fcd0f21c63cba9a134d00d8c4
SaamiJ <saamijunaidi@gmail.com>
2025-08-16 22:49:36 +1000

    Fixing merge conflicts

--- stm/Debug/Core/Src/stm32l4xx_it.d
+++ stm/Debug/Core/Src/stm32l4xx_it.d
@@ -27,7 +27,6 @@ Core/Src/stm32l4xx_it.o: ../Core/Src/stm32l4xx_it.c ../Core/Inc/main.h \
  ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim_ex.h \
  ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h \
  ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h \
- ../Core/Inc/motor_control.h ../Core/Inc/main.h \
  ../Core/Inc/stm32l4xx_it.h
 ../Core/Inc/main.h:
 ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h:
@@ -58,6 +57,4 @@ Core/Src/stm32l4xx_it.o: ../Core/Src/stm32l4xx_it.c ../Core/Inc/main.h \
 ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim_ex.h:
 ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h:
 ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h:
-../Core/Inc/motor_control.h:
-../Core/Inc/main.h:
 ../Core/Inc/stm32l4xx_it.h:
