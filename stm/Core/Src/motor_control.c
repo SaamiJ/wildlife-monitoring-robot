a3e98e241efa1997194fdb211e08e3e23ba058f7
SaamiJ <111267497+SaamiJ@users.noreply.github.com>
2025-08-30 18:00:17 +1000

    Updated stm32 firmware

--- stm/Core/Src/motor_control.c
+++ stm/Core/Src/motor_control.c
@@ -38,7 +38,7 @@ void Motor_StopAll(void)
 {
     /* Coast both motors: INx low, PWM = 0 */
     HAL_GPIO_WritePin(MOTOR_GPIO_PORT, MOTOR_LEFT_IN1_PIN|MOTOR_LEFT_IN2_PIN|
-                                         MOTOR_RIGHT_IN3_PIN|MOTOR_RIGHT_IN4_PIN, GPIO_PIN_RESET);
+                                         MOTOR_RIGHT_IN3_PIN|MOTOR_RIGHT_IN4_PIN, GPIO_PIN_SET);
     __HAL_TIM_SET_COMPARE(&htim1, MOTOR_LEFT_PWM_CH, 0);
     __HAL_TIM_SET_COMPARE(&htim1, MOTOR_RIGHT_PWM_CH, 0);
 }
